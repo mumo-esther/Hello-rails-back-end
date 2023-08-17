da53186b2ec55151934e5c898f778515b6a04543
mumo-esther <mumoesther2021@gmail.com>
2023-08-17 14:18:06 +0300

    Config ruby on rails linters

--- test/controllers/greetings_controller_test.rb
+++ test/controllers/greetings_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class GreetingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get random_greetings" do
+  test 'should get random_greetings' do
     get greetings_random_greetings_url
     assert_response :success
   end
