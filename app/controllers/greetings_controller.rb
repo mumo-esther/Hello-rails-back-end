e68c3cda89eb8707a3ee957b13bfdb15b3c3d3be
mumo-esther <mumoesther2021@gmail.com>
2023-08-17 14:08:56 +0300

    Made random-greetings global

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,6 +1,6 @@
 class GreetingsController < ApplicationController
   def random_greetings
-    random_message = Message.order('RANDOM()').first
-    render json: { greeting: random_message.text }
+    @random_message = Message.order('RANDOM()').first
+    render json: { greeting: @random_message.text }
   end
 end
