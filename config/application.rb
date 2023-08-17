b30920e34da77fe2e7c4b5b561bd430696499cc8
mumo-esther <mumoesther2021@gmail.com>
2023-08-17 14:09:08 +0300

    Add cors configuration

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,13 @@ module HelloRailsBackEnd
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    
+    config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://localhost:3001' # The origin of your React app
+      resource '*', headers: :any, methods: %i[get post put patch delete options]
+    end
+  end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
